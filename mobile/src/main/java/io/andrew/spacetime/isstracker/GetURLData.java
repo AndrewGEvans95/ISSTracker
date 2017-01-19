63d695380d2c11ecb2c401e678b25d0692944d96
AndrewGEvans95 <Andrewgevans95@gmail.com>
2017-01-19 14:41:46 -0500

    Initial commit

---
+++ mobile/src/main/java/io/andrew/spacetime/isstracker/GetURLData.java
@@ -0,0 +1,88 @@
+package io.andrew.spacetime.isstracker;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Andrew on 1/18/2017.
+ */
+
+public class GetURLData{
+
+  public TestAsyncTask testAsyncTask;
+  public String allData = "";
+
+
+  public GetURLData(MainActivity ma){
+    testAsyncTask = new TestAsyncTask(ma, "http://api.open-notify.org/iss-now.json/");
+    Log.e("Async", "Started");
+  }
+
+  public void startAsync(){
+    testAsyncTask.execute();
+  }
+
+  public class TestAsyncTask extends AsyncTask<Void, Void, String> {
+    private Context mContext;
+    private String mUrl;
+
+    public TestAsyncTask(Context context, String url) {
+      mContext = context;
+      mUrl = url;
+    }
+
+    @Override protected void onPreExecute() {
+      super.onPreExecute();
+    }
+
+    @Override protected String doInBackground(Void... params) {
+      String resultString = null;
+      resultString = getJSON(mUrl);
+      allData = resultString;
+      return resultString;
+    }
+
+    @Override protected void onPostExecute(String strings) {
+      super.onPostExecute(strings);
+      Log.e("GetURLData", "Data retrieved: " + allData);
+    }
+
+    private String getJSON(String iUrl) {
+      URL url;
+      HttpURLConnection urlConnection = null;
+      String output = "";
+      Log.e("Async", "Attempting connection with: " + iUrl);
+      try {
+        url = new URL(iUrl);
+
+        urlConnection = (HttpURLConnection) url.openConnection();
+
+        InputStream in = urlConnection.getInputStream();
+
+        InputStreamReader isw = new InputStreamReader(in);
+
+        int data = isw.read();
+        Log.e("Website data", "Retrieving data");
+        while (data != -1) {
+          char current = (char) data;
+          data = isw.read();
+          //Log.e("Website data", toString(current));
+          output = output + current;
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        if (urlConnection != null) {
+          urlConnection.disconnect();
+        }
+      }
+      return output;
+    }
+  }
+}
---
+++ mobile/src/main/java/io/andrew/spacetime/isstracker/GoogleClientHandler.java
@@ -0,0 +1,91 @@
+package io.andrew.spacetime.isstracker;
+
+import android.content.Context;
+import android.util.Log;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Andrew on 1/18/2017.
+ */
+
+public class GoogleClientHandler extends Thread{
+  private static final long CONNECTION_TIME_OUT_MS = 1000;
+
+  public GoogleApiClient client;
+  public String nodeId;
+  public Thread rThread;
+
+  public GoogleClientHandler(){
+
+  }
+
+  /**
+   * Returns a GoogleApiClient that can access the Wear API.
+   * @param context
+   * @return A GoogleApiClient that can make calls to the Wear API
+   */
+  public GoogleApiClient getGoogleApiClient(Context context) {
+    return new GoogleApiClient.Builder(context)
+        .addApi(Wearable.API)
+        .build();
+  }
+
+  /**
+   * Connects to the GoogleApiClient and retrieves the connected device's Node ID. If there are
+   * multiple connected devices, the first Node ID is returned.
+   */
+  public void retrieveDeviceNode() {
+    rThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Log.e("retrieveDeviceNode", "Looking for device");
+        client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+        NodeApi.GetConnectedNodesResult result =
+            Wearable.NodeApi.getConnectedNodes(client).await();
+        List<Node> nodes = result.getNodes();
+        if (nodes.size() > 0) {
+          Log.e("retrieveDeviceNode", "Device found: " + nodes.get(0).getDisplayName());
+          nodeId = nodes.get(0).getId();
+          //Send toast now
+          sendData();
+        }
+        else{
+          Log.e("retrieveDeviceNode", "No devices found");
+        }
+        Log.e("retrieveDeviceNode", "disconnecting from client");
+        client.disconnect();
+      }
+    });
+    rThread.start();
+  }
+
+  public void sendData() {
+    Log.e("sendToast", "sendToast called");
+    if (nodeId != null) {
+      Log.e("sendToast", "node detected");
+      new Thread(new Runnable() {
+        @Override
+        public void run() {
+          client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
+          Wearable.MessageApi.sendMessage(client, nodeId, "Message to be sent", null);
+          Log.e("sendToast", "toast sent");
+          client.disconnect();
+        }
+      }).start();
+    }
+  }
+
+  public String getNodeId(){
+    return nodeId;
+  }
+
+  public void updateClient(GoogleApiClient cT){
+    client=cT;
+  }
+
+}
